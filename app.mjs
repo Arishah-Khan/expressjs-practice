879c45b316c8fa15b74ea9fd872dc3117b12c791
Arishah Khan <arishahsanaullah78@gmail.com>
2025-02-19 10:31:19 +0500

    add frontend

--- app.mjs
+++ app.mjs
@@ -1,13 +1,33 @@
-import express from 'express';
+import express from "express";
+import path from "path";  
+const app = express();
+const port = process.env.PORT || 3000;
 
+const _dirname = path.resolve()
 
-const app = express()
-const port = process.env.PORT || 3000
+app.use('/' , express.static(path.join(_dirname,'./web/dist')))
+
 
 app.get('/', (req, res) => {
-  res.send('Hello Server!')
-})
+  res.send("Home page served from the dist directory");
+});
+
+app.get('/weather', (req, res) => {
+  res.json({ user: 'tobi' });
+});
+
+app.get('/weather/:cityName', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/weather/:cityName/:side', (req, res) => {
+  res.send(req.params);
+});
+
+app.use("/", (req, res) => {
+  res.send("Middleware is running");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
